3e7ee14cbde6ed770a85ab13e8b3bb6832b14159
nicolaschautard <nicolas43770@gmail.com>
2020-04-14 16:59:00 +0200

    add missing organization filter on slides query

--- src/Layout/DataProvider/SliderDataProvider.php
+++ src/Layout/DataProvider/SliderDataProvider.php
@@ -4,15 +4,14 @@ namespace SliderBundle\Layout\DataProvider;
 
 use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\ScopeBundle\Manager\ScopeManager;
+use Oro\Bundle\SecurityBundle\Authentication\TokenAccessorInterface;
 use SliderBundle\Entity\Slider;
 use SliderBundle\Entity\Repository\SliderRepository;
 use SliderBundle\Slide\SlideResolver;
 
 class SliderDataProvider
 {
-    /**
-     * @var SliderRepository
-     */
+    /** @var SliderRepository */
     protected $sliderRepository;
 
     /** @var ConfigManager */
@@ -24,50 +23,58 @@ class SliderDataProvider
     /** @var SlideResolver */
     protected $slideResolver;
 
+    /** @var TokenAccessorInterface */
+    protected $tokenAccessor;
+
     /**
      * SliderDataProvider constructor.
      * @param SliderRepository $sliderRepository
      * @param ConfigManager $config
      * @param ScopeManager $scopeManager
      * @param SlideResolver $slideResolver
+     * @param TokenAccessorInterface $tokenAccessor
      */
-    public function __construct(SliderRepository $sliderRepository, ConfigManager $config, ScopeManager $scopeManager, SlideResolver $slideResolver)
+    public function __construct(SliderRepository $sliderRepository, ConfigManager $config, ScopeManager $scopeManager, SlideResolver $slideResolver, TokenAccessorInterface $tokenAccessor)
     {
         $this->sliderRepository = $sliderRepository;
         $this->config = $config;
         $this->scopeManager = $scopeManager;
         $this->slideResolver = $slideResolver;
+        $this->tokenAccessor= $tokenAccessor;
     }
-
 
     /**
      * @var $string
-     * @return Slider $slider
+     * @return array $slider
      */
     public function getSlidesBySliderCode($sliderCode)
     {
         $criteria = $this->scopeManager->getCriteria('slide');
         $context = $criteria->toArray();
-        $slides = $this->sliderRepository->getSlidesBySliderCode($sliderCode);
-        $slidesVisibles = $this->slideResolver->getVisibleSlides($slides,$context);
-        return $slidesVisibles;
+        $slides = $this->sliderRepository->getSlidesBySliderCode($sliderCode, $this->tokenAccessor->getOrganization());
+        $slidesVisibles = $this->slideResolver->getVisibleSlides($slides, $context);
+        if (!is_null($slidesVisibles)) {
+            return $slidesVisibles;
+        }
+        return;
     }
 
     public function getSliderUsedConfig()
     {
         /** @var Slider $slider */
         $sliderId = $this->config->get('slider_bundle.slider');
-        return  $this->sliderRepository->find($sliderId);
+        if (!is_null($sliderId)) {
+            return $this->sliderRepository->find($sliderId);
+        }
+        return;
     }
 
     public function getSliderCodeUsedConfig()
     {
-        $sliderId = $this->config->get('slider_bundle.slider');
-        
-        /** @var Slider $slider */
-        $slider = $this->sliderRepository->find($sliderId);
-        
-        return $slider->getCode();
+        $slider = $this->getSliderUsedConfig();
+        if (!is_null($slider)) {
+            return $slider->getCode();
+        }
+        return;
     }
-
 }
